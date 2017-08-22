47f70d325bccec769d775cac632a7cddfd382c19
Jenner Hanni <jenner@wickerbox.net>
2017-08-22 12:23:38 -0700

    All syms point at valid footprints (even if UNDEFINED)

--- libraries/fpcheck.py
+++ libraries/fpcheck.py
@@ -11,9 +11,12 @@ fplibdir = "/home/wicker/wickerlib/libraries/Wickerlib.pretty/"
 modules = glob.glob(fplibdir+'*.kicad_mod')
 modules = sorted(modules)
 
+fp_list = []
+
 for fp in modules:
   fp = fp.replace("/home/wicker/wickerlib/libraries/Wickerlib.pretty/","")
   fp = fp.replace(".kicad_mod","")
+  fp_list.append(fp)
   #print fp
 
 # collect a list of all the symbol names
@@ -23,44 +26,13 @@ symlist = []
 
 with open(symlibfile, 'r') as symfile:
   for line in  symfile: 
-    if 'ENDDEF' not in line:
-      if 'DEF ' in line:
-        m_def = '' 
-        m_fp = ''
-        m_def = line.split(' ')[1]
-      if 'F2 ' in line:
-        m_fp = line.split(' ')[1].replace("\"","")
-        if m_fp != '':
-          symlist.append((m_def,m_fp))
-      
-for sym in symlist:
-
-  if 'Wickerlib' not in sym[1]:
-    print sym[0]+'            '+sym[1]
-
-## search all files in the glob for the verified status
-#
-#for module_path in modules:
-#  with open(module_path, 'r') as module:
-#    for line in module:
-#      if 'Verified' in line:
-#        line_verify = line.lstrip('# Verified: ').rstrip('\n')
-#      if 'Finished' in line:
-#        line_finish = line.lstrip('# Finished: ').rstrip('\n')
-#    mod_name = module_path.lstrip(libdir).rstrip('.kicad_mod')
-#    liblist_output.append('| '+mod_name+' | '+line_verify+' | '+line_finish+' |')
-#
-#outfile_path = 'README.md'
-#today = datetime.datetime.today()
-#dt = today.strftime("%d %B %Y")
-#
-#with open(outfile_path,'w') as o:
-#  o.write("# Wickerlib.pretty KiCad Module (Footprint) Library")
-#  o.write("\nThese modules have been edited to have fabrication information for wickerlib. They all come with comments in the headers of each that includes attribution, the appropriate license, and whether the footprint has been used successfully in a project.")
-#  o.write("\nIt is always the end user's responsibility to verify the package.")
-#  o.write("\n\nThis list is updated each time the repository is updated.")
-#  o.write("\n\nLast updated: "+dt)
-#  o.write("\n\n|Module Name|Verified|Finished|")
-#  o.write("\n|------|--------|--------|")
-#  for l in liblist_output:
-#    o.write("\n"+l)
+    line = line.split(' ')
+    if 'DEF' == line[0]:
+      m_def = '' 
+      m_fp = ''
+      m_def = line[1]
+    if 'F2' in line[0]:
+      m_fp = line[1].replace("\"","")
+      if 'UNDEFINED' in m_fp:
+        print m_def
+    
