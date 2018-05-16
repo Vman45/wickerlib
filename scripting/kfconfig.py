87f6f63245a698b5e1e0ff727be6f66f992d1d61
Jenner Hanni <jenner@wickerbox.net>
2018-05-15 17:41:41 -0700

    Remove KiFisher to separate project

--- scripting/kfconfig.py
+++
@@ -1,9 +0,0 @@
-# this is a configuration file 
-# for kingfisher.py
-
-templates_dir = '/home/wicker/wickerlib/templates/'
-
-default_assembly_image_width = 50
-default_schematic_image_width = 50
-default_preview_image_width = 50
-default_other_image_width = 50
